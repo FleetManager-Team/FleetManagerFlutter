0af5d7012f22d3f1b685c4383daa3bf9c8218a0a
basisfrancesco <francesco@basis.bg.it>
2026-04-03 07:59:25 +0000

    Deploying to gh-pages from @ FleetManager-Team/FleetManagerFlutter@89cdb5fc8c62e5dd9a4f96c4364fb10cf271cc56 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2692350629" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1892798445" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
