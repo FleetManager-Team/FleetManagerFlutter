b57626d4310cfd4ff8b7923aee223085526ccccd
platipietro-web <plati.pietro@gmail.com>
2026-04-09 16:25:33 +0000

    Deploying to gh-pages from @ FleetManager-Team/FleetManagerFlutter@fc111c92d7c9fb23580bdb23680556c38f632aec 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2798613381" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "221453572" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
